02f8cbb15b168131e747bae3b25d104ee91c3390
abeerhamdi7 <95494507+abeerhamdi7@users.noreply.github.com>
2022-03-06 19:19:47 +0200

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,64 @@
+
+    const requireEmail=document.querySelector(".require-email");
+    const form = document.getElementById('form');
+    const username = document.getElementById('name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    
+    
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+    
+    });
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*ar name=document.myform.name.value;
+var email=document.myform.email.value;
+var password=document.myform.password.value;
+
+//validation function(){
+    function validate()
+{ 
+    if(name.value =="" || (name.value == null)
+    {
+        alert("name can't be blank")
+        
+        return false;
+    }
+    else if(email.value ==""|| email.value == null)
+    {
+        email.style.border="1px solid red";
+        email_error.textContent="user name is required";
+        return false;
+    }
+    else if(password.value ==""|| password.value == null)
+    {
+        password.style.border="1px solid red";
+        password_error.textContent="user name is required";
+        return false;
+    }
+}
+
+
+/----arrow top------ 
+var topArrow=document.querySelector('#arrow-top')
+window.onscroll = function() {
+    console.log('window is scrolling')
+    if(document.documentElement.scrollTop > 200) topArrow.style.display="block"
+    else topArrow.style.display="none" */
+    
+
